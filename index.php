6550c7179f223b38e397a8075274b2a0a80d9c05
Genival dos Santos Furtado Junior <moreno.alado@gmail.com>
2013-02-25 10:24:50 -0300

    Iniciado

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include("header.php") ?>
+<?php include("topo.php") ?>
+    <title>NAHUM</title>
+  </head>
+  <body>
+    
+    <div id="content">
+    </div>
+    <div id="navigation">
+    </div>
+    <div id="sidebar">
+    </div>
+    <div id="footer">
+    <?php $data=date("Y"); echo $data; ?> <!-- Data Atual -->
+    </div>
+  </body>
+</html>
+
---
+++ topo.php
@@ -0,0 +1,19 @@
+
+	    <div id="header" class="centraliza">
+
+        	<p class="texto_topo">Todos os itens de luxo, no 2 Quartos,<br/>
+        	 	com o melhor preço da categoria.</p>
+        	<img src="images/topo_cont.png" alt="" class="image">   
+  		</div>
+        
+        
+			<div id="vendas">
+		       <p class="vend">VENDAS</p>
+		        <hr>
+		        <p>(091) 3277 - 2960</p>
+		        <p>(091) 3228 - 0875</p>	        	
+			</div>
+
+
+
+
